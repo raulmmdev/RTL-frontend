9609d7724cd75105ce261e3cb7b233cab43e9448
Raul Mena <raulmenamontes@gmail.com>
2020-03-07 19:46:39 +0100

    Add types of components

---
+++ src/pages/types/Homepage.types.ts
@@ -0,0 +1,40 @@
+import { TVMazeShow } from "./TVShow.types";
+import { TVMazeShowEpisodes } from "./TVShowEpisodes.types";
+import { TVMazeShowEpisodeDetail } from "./TVShowEpisodeDetail.types";
+
+export interface FilteredSeason {
+  [key: number]: TVMazeShowEpisodes[];
+}
+
+export interface State {
+  season: number;
+}
+
+export interface Props {
+  view: string;
+  fetchTVShows: (id: string) => void;
+  fetchTVShowEpisodes: (id: string) => Promise<TVMazeShowEpisodes>;
+  fetchTVShowsEpisodeDetail: (
+    id: string,
+    season: string,
+    episode: string
+  ) => Promise<TVMazeShowEpisodeDetail>;
+  show: Show;
+  match: MatchRouter;
+}
+
+interface MatchRouter {
+  params: ParamsMatch;
+}
+
+interface ParamsMatch {
+  number?: string;
+  season?: string;
+  id?: string;
+}
+
+interface Show {
+  tvShow: TVMazeShow;
+  episodes: TVMazeShowEpisodes[];
+  episodeDetail?: TVMazeShowEpisodeDetail;
+}
